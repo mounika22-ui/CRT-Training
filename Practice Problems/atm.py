6f3334dbe215ef27f690cb0eb88d857e01c38488
Mounika <mounikasista95@gmail.com>
2026-06-20 10:45:59 +0530

    Updated atm.py

--- Practice Problems/atm.py
+++ Practice Problems/atm.py
@@ -7,17 +7,17 @@ then show withdraw sucessful if amount is less show it as amount is in sufficien
 initial balance = 5000
 '''
 
-atmpin=2005
-balance =5000
-pin=int(input("Enter the pin"))
-if pin==atmpin:
-    withdraw=int(input("Withdrawing amount"))
-    if balance>=withdraw:
-        balance=balance-withdraw
-        finalbalance= balance
-        print("Withdraw is successfull")
-        print("final balance ",finalbalance)
-    else:
-        print("Insufficient balance")
-else:
-    print("Invaild pin")        
+# atmpin=2005
+# balance =5000
+# pin=int(input("Enter the pin"))
+# if pin==atmpin:
+#     withdraw=int(input("Withdrawing amount"))
+#     if balance>=withdraw:
+#         balance=balance-withdraw
+#         finalbalance= balance
+#         print("Withdraw is successfull")
+#         print("final balance ",finalbalance)
+#     else:
+#         print("Insufficient balance")
+# else:
+#     print("Invaild pin")        
